6212c461ac2203f26a8a8c778c7d19809fa7682d
Xia junwe <jxia27@binghamton.edu>
2021-09-15 10:07:41 -0400

    Forth commit

---
+++ app/src/main/java/com/example/myapplication/ThirdActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class ThirdActivity extends Activity {
+    private VideoView videoView;
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);   //全屏
+        play_mp4();
+
+    }
+    private void play_mp4() {
+        String videoUrl1 = "android.resource://" + getPackageName() + "/" + R.raw.yuanshen1;
+        Uri uri = Uri.parse( videoUrl1 );
+        videoView = (VideoView) findViewById(R.id.videoView);
+
+        videoView.setMediaController(new MediaController(this));
+        videoView.setVideoURI(uri);
+        videoView.start();
+    }
+}
